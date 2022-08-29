31545171ae94d63c82904674030a8edf886044d2
iOSHiepLe <hieplemobile@gmail.com>
2022-08-29 12:59:33 +0700

    test

--- app/src/main/java/com/example/testpackage/MainActivity.kt
+++ app/src/main/java/com/example/testpackage/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.testpackage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.tixngo_manager.tixngo.TixngoManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
